b8a5d944791f82adf2c37b0985f30ada875b54a4
Iadolphe007 <iadolphe007@gmail.com>
2023-07-25 11:20:44 +0200

    added func

--- script.js
+++ script.js
@@ -2,16 +2,19 @@ const display = document.querySelector('.screen')
 const buttons = document.querySelectorAll('button')
 let result = ''
 const operator = ["%", "*", "/", "-", "+", "="]
-
  
 const operation = (btn_value) => {
     if (btn_value === "=" && result !== " "){
+        
         result = eval(result.replace("%", "/100"))
+
     }else if(btn_value === "AC"){
         result = ''
-    }else if (btn_value === 'Del'){
+    }
+    
+    else if (btn_value === 'Del'){
         result = result.toString().slice(0, -1)
-    }else{
+    } else{
         if(result === '' && operator.includes(btn_value)) return
         result += btn_value
     }
